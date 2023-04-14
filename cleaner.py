88b6979a7dd384a48264a3db1045b10d8d6b2585
Raulacinherrero <acin.raul@gmail.com>
2023-04-14 09:19:20 +0200

    refactor cleaner

--- cleaner.py
+++ cleaner.py
@@ -3,10 +3,10 @@ from functions.delete_dir import delete_dir
 
 fs_path = os.path.dirname(os.path.abspath(__file__))
 web_path = os.path.join(fs_path, 'web')
-backo_oath = os.path.join(fs_path, 'backoffice')
+backo_path = os.path.join(fs_path, 'backoffice')
 
 if os.path.exists(web_path):
     delete_dir(web_path)
 
-if os.path.exists(backo_oath):
-    delete_dir(backo_oath)
+if os.path.exists(backo_path):
+    delete_dir(backo_path)
