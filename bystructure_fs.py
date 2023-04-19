e23ec0fb96aca15bab20850f029d62c73544d23a
Raulacinherrero <acin.raul@gmail.com>
2023-04-19 08:57:47 +0200

    --web y --backoffice implementados

--- bystructure_fs.py
+++ bystructure_fs.py
@@ -15,106 +15,75 @@ rs_path = os.path.join(fs_path, 'functions/read_history.py')
 parser = argparse.ArgumentParser()
 parser.add_argument('--force', help='Ejecutar una operación forzada', action='store_true')
 parser.add_argument('--show', help='Muestra toda la salida', action='store_true')
+parser.add_argument('--web', help='Crea solo los archivos que se crean en web', action='store_true')
+parser.add_argument('--backoffice', help='Crea solo los archivos que se crean en backoffice', action='store_true')
 args = parser.parse_args()
 
 sys.stdout = open(his_path, 'w')
 
-if args.force:
-    with open(md_path, 'r') as structure:
-        for linea in structure:
-            page_match = re.search(r'^# \*\*(\w+)\*\*$', linea)
-            if page_match:
-                camelName = page_match.group(1)
-                fileseeder("gpag", camelName, None, True)
-                fileseeder("land", camelName, None, True)
-                fileseeder("sdoc", camelName, None, True)
-                fileseeder("gpag", camelName)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-            template_match = re.search(r'^# (\w+)$', linea)
-            if template_match:
-                camelName = template_match.group(1)
-                fileseeder("gtemp", camelName, None, True)
-                fileseeder("land", camelName, None, True)
-                fileseeder("sdoc", camelName, None, True)
-                fileseeder("gtemp", camelName)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-            organism_match = re.search(r'^## (\w+)$', linea)
-            if organism_match:
-                camelName = organism_match.group(1)
-                fileseeder("org", camelName, None, True)
-                fileseeder("sobj", camelName, None, True)
-                fileseeder("org", camelName)
-                fileseeder("sobj", camelName)
-            molecule_match = re.search(r'^### (\w+)$', linea)
-            if molecule_match:
-                camelName = molecule_match.group(1)
-                fileseeder("mol", camelName, None, True)
-                fileseeder("mol", camelName)
-            page_match_traduccion = re.search(r'^# \*\*(\w+)\*\* - ', linea)
+with open(md_path, 'r') as structure:
+    for linea in structure:
+        page_match = re.search(r'^# \*\*(\w+)\*\*', linea)
+        page_match_default = re.search(r'^# \*\*(\w+)\*\*$', linea)
+        page_match_traduccion = re.search(r'^# \*\*(\w+)\*\* - ', linea)
+        if page_match:
+            camelName = page_match.group(1)
             if page_match_traduccion:
-                camelName = page_match_traduccion.group(1)
                 kebabTraduccion = linea.split(' - ')[1]
-                fileseeder("gpag", camelName, kebabTraduccion, True, True)
-                fileseeder("land", camelName, None, True)
-                fileseeder("sdoc", camelName, None, True)
-                fileseeder("gpag", camelName, kebabTraduccion, False, True)
+            if args.backoffice == False:
+                build_temp_files(camelName)
+                if args.force:
+                    fileseeder("gpag", camelName, None, True)
+                    fileseeder("land", camelName, None, True)
+                if page_match_traduccion:
+                    fileseeder("gpag", camelName, kebabTraduccion, False, True)
+                if page_match_default:
+                    fileseeder("gpag", camelName)
                 fileseeder("land", camelName)
+            if args.web == False:
+                if args.force:
+                    fileseeder("sdoc", camelName, None, True)
                 fileseeder("sdoc", camelName)
-            template_match_traduccion = re.search(r'^# (\w+) - ', linea)
+        template_match = re.search(r'^# (\w+)', linea)
+        template_match_default = re.search(r'^# (\w+)$', linea)
+        template_match_traduccion = re.search(r'^# (\w+) - ', linea)
+        if template_match:
+            camelName = template_match.group(1)
             if template_match_traduccion:
-                camelName = template_match_traduccion.group(1)
                 kebabTraduccion = linea.split(' - ')[1]
-                fileseeder("gtemp", camelName, kebabTraduccion, True, True)
-                fileseeder("land", camelName, None, True)
-                fileseeder("sdoc", camelName, None, True)
-                fileseeder("gtemp", camelName, kebabTraduccion, False, True)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-else:
-    with open(md_path, 'r') as structure:
-        for linea in structure:
-            page_match = re.search(r'^# \*\*(\w+)\*\*$', linea)
-            if page_match:
-                camelName = page_match.group(1)
+            if args.backoffice == False:
                 build_temp_files(camelName)
-                fileseeder("gpag", camelName)
+                if args.force:
+                    fileseeder("gtemp", camelName, None, True)
+                    fileseeder("land", camelName, None, True)
+                if template_match_traduccion:
+                    fileseeder("gtemp", camelName, kebabTraduccion, False, True)
+                if template_match_default:
+                    fileseeder("gtemp", camelName)
                 fileseeder("land", camelName)
+            if args.web == False:
+                if args.force:
+                    fileseeder("sdoc", camelName, None, True)
                 fileseeder("sdoc", camelName)
-            template_match = re.search(r'^# (\w+)$', linea)
-            if template_match:
-                camelName = template_match.group(1)
-                build_temp_files(camelName)
-                fileseeder("gtemp", camelName)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-            organism_match = re.search(r'^## (\w+)$', linea)
-            if organism_match:
-                camelName = organism_match.group(1)
+        organism_match = re.search(r'^## (\w+)$', linea)
+        if organism_match:
+            camelName = organism_match.group(1)
+            if args.backoffice == False:
+                if args.force:
+                    fileseeder("org", camelName, None, True)
                 fileseeder("org", camelName)
+            if args.web == False:
+                if args.force:
+                    fileseeder("sobj", camelName, None, True)
                 fileseeder("sobj", camelName)
-            molecule_match = re.search(r'^### (\w+)$', linea)
-            if molecule_match:
-                camelName = molecule_match.group(1)
+        molecule_match = re.search(r'^### (\w+)$', linea)
+        if molecule_match:
+            camelName = molecule_match.group(1)
+            if args.backoffice == False:
+                if args.force:
+                    fileseeder("mol", camelName, None, True)
                 fileseeder("mol", camelName)
-            page_match_traduccion = re.search(r'^# \*\*(\w+)\*\* - ', linea)
-            if page_match_traduccion:
-                camelName = page_match_traduccion.group(1)
-                build_temp_files(camelName)
-                kebabTraduccion = linea.split(' - ')[1]
-                fileseeder("gpag", camelName, kebabTraduccion, False, True)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-            template_match_traduccion = re.search(r'^# (\w+) - ', linea)
-            if template_match_traduccion:
-                camelName = template_match_traduccion.group(1)
-                build_temp_files(camelName)
-                kebabTraduccion = linea.split(' - ')[1]
-                fileseeder("gtemp", camelName, kebabTraduccion, False, True)
-                fileseeder("land", camelName)
-                fileseeder("sdoc", camelName)
-
+                
 sys.stdout.close()
 
 if args.show:
