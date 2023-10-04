4cf3dfc07f7a9139b2822fcaff51b585d69693ca
Alberto <albertolahuerta@gmail.com>
2023-10-04 15:02:26 +0200

    No encontraba el layout

--- bystructure_fs.py
+++ bystructure_fs.py
@@ -42,21 +42,21 @@ with open(md_path, 'r') as structure:
             fileseeder("sdoc-prototype", camelName)
             fileseeder("land", camelName)
             fileseeder("gprototype", camelName)
-#         if page_match:
-#             ndir = 0
-#             if args.backoffice == False:
-# #                 build_temp_files(camelName, ndir)
-#                 if args.force:
-#                     if page_match_default:
-#                         fileseeder("gpag", camelName, None, True)
-#                     fileseeder("land", camelName, None, True)
-#                 if page_match_default:
-#                     fileseeder("gpag", camelName)
-#                 fileseeder("land", camelName)
-#             if args.web == False:
-#                 if args.force:
-#                     fileseeder("sdoc", camelName, None, True)
-#                 fileseeder("sdoc", camelName)
+        #         if page_match:
+        #             ndir = 0
+        #             if args.backoffice == False:
+        # #                 build_temp_files(camelName, ndir)
+        #                 if args.force:
+        #                     if page_match_default:
+        #                         fileseeder("gpag", camelName, None, True)
+        #                     fileseeder("land", camelName, None, True)
+        #                 if page_match_default:
+        #                     fileseeder("gpag", camelName)
+        #                 fileseeder("land", camelName)
+        #             if args.web == False:
+        #                 if args.force:
+        #                     fileseeder("sdoc", camelName, None, True)
+        #                 fileseeder("sdoc", camelName)
 
         # template_match_traduccion = re.search(r'^# (\w+) - ', linea)
         organism_match = re.search(r'^## (\w+)$', linea)
@@ -78,17 +78,18 @@ with open(md_path, 'r') as structure:
                 if args.force:
                     fileseeder("mol", camelName, None, True)
                 fileseeder("mol", camelName)
-                
-sys.stdout.close()
+
 build_temp_files_sanity()
 fs_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+sdoc_path = os.path.join(root_path, 'backoffice/schemas')
 template_index = os.path.join(fs_path, "templates/Sanity-Index.ts")
-all_imports_sanity_path = os.path.join(fs_path, 'temp_all_imports_sanity_fs.ts')
-documents_n_objects_fields_path = os.path.join(fs_path, 'temp_documents_n_objects_fields_fs.ts')
-
+all_imports_sanity_path = os.path.join(root_path, 'temp_all_imports_sanity_fs.ts')
+documents_n_objects_fields_path = os.path.join(root_path, 'temp_documents_n_objects_fields_fs.ts')
+print(f'{template_index} Va a ser creado')
 with open(template_index, 'r') as template_ts:
     code = template_ts.read()
-    if os.path.exists(all_imports_sanity_path) and os.path.exists(documents_n_objects_fields_path) :
+    if os.path.exists(all_imports_sanity_path) and os.path.exists(documents_n_objects_fields_path):
+        print(f'ya existe')
         with open(all_imports_sanity_path, 'r') as tempImportLine:
             imports = tempImportLine.read().strip()
         with open(documents_n_objects_fields_path, 'r') as tempDocsObjsLine:
@@ -97,9 +98,11 @@ with open(template_index, 'r') as template_ts:
         code = code.replace('${OBJECTS}', docs_n_objs)
         os.remove(all_imports_sanity_path)
         os.remove(documents_n_objects_fields_path)
-        file_path = os.path.join(root_path, "backoffice/schemas/schema.ts")
+        file_path = os.path.join(os.path.join(sdoc_path, "schema.ts"))
         with open(file_path, 'w') as new_file:
             new_file.write(code)
+
+sys.stdout.close()
 if args.show:
     subprocess.call(["python3", rs_path , "--show" ])
 else:
