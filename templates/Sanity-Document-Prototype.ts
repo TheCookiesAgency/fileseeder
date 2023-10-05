dc351b43b215f627a12e77879b5801ba0efbcbc0
Alberto <albertolahuerta@gmail.com>
2023-10-05 16:28:07 +0200

    faltaban grupos

--- templates/Sanity-Document-Prototype.ts
+++ templates/Sanity-Document-Prototype.ts
@@ -6,6 +6,12 @@ export default defineType({
   name: '${NAME}',
   title: '${NAME}',
   type: 'document',
+  groups: [
+    { name: "seo", title: "Metas" },
+    { name: "content", title: "Contenido" },
+    { name: "shared", title: "Compartido" },
+    { name: "settings", title: "Configuración" },
+  ],
   fields: [
       ...prototype,
     ${FIELDS}],
