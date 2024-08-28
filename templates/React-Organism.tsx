59c9821962551b024539e15cdd85636508bf80f6
alberto <albertolahuerta@gmail.com>
2024-08-28 16:18:06 +0200

    added copy

--- templates/React-Organism.tsx
+++ templates/React-Organism.tsx
@@ -1,14 +1,22 @@
 ---
 import {sanityClient} from "sanity:client";
-import type { ${NAME} } from "@/shared/sanity/sanity.types";
+import type { ${NAME} } from "@/shared/sanity/sanity.types"
+import {getHtmlSimpleCopy} from "@/shared/utils/get-html-render";
+
+import type {TagTitle} from "@/shared/utils/get-html-render";
 
 
-const {  _id} = Astro.props;
+interface Props {
+    _id: string;
+    headingLevel?: TagTitle
+}
+
+const {  _id, headingLevel } = Astro.props;
 // change firsLetter to lowercase in query
 const data = await sanityClient.fetch<${NAME}>(
     `*[_id == "${_id}" ][0]{ ...${lowerCamelName}}`,
 );
-
+const copy = getHtmlSimpleCopy(data.copy, headingLevel );
  ---
 <section class={`o-${className}`}>
   <div class={`c-${className}`}>
