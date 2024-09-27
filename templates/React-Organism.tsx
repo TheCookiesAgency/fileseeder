4c4b78e161332f3c959a858fb28031c8ca89fa8a
alberto <albertolahuerta@gmail.com>
2024-09-27 22:23:02 +0200

    mejora de organismo

--- templates/React-Organism.tsx
+++ templates/React-Organism.tsx
@@ -1,5 +1,4 @@
 ---
-import type { ${NAME} } from "@/shared/sanity/sanity.types"
 import {getHtmlSimpleCopy} from "@/shared/utils/get-html-render";
 
 import type {TagTitle} from "@/shared/utils/get-html-render";
@@ -7,17 +6,21 @@ import type {TagTitle} from "@/shared/utils/get-html-render";
 
 interface Props {
     _id: string;
+    collection: CollectionName;
     headingLevel?: TagTitle
 }
 
-const {  _id, headingLevel } = Astro.props;
-const {data}: Data<{ ${lowerCamelName}: ${NAME}}>  = await getEntry("${NAME}", _id);
-
+const {  _id, headingLevel, collection } = Astro.props;
+const entry: Data<{ ${lowerCamelName}: ${NAME}}>  = await getEntry(collection, _id);
+const data = entry.data.${lowerCamelName} as ${NAME}Entry;
 const copy = getHtmlSimpleCopy(data.copy, headingLevel );
+
  ---
+
 <section class={`o-${className}`}>
-  <div class={`c-${className}`}>
-      <h1>Hola! soy ${NAME},</h1>
-      <pre>{JSON.stringify(data,  null, 2)}</pre>
-  </div>
+    <div class={`c-${className}`}>
+        <h1>Hola! soy ${NAME},</h1>
+        <div class={`${className}__copy`} set:html={copy}/>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
 </section>
